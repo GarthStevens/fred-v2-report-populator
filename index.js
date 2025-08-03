9cd7706c0f282c1b6e7eb43d2b29a92766192be1
Garth Stevens <garth.stevens@live.com.au>
2025-08-03 20:41:01 +1000

    wip

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const hubspot = require("@hubspot/api-client");
 const fs = require("fs");
 const dotenv = require("dotenv");
-const { parse, formatISO } = require("date-fns");
+const { parse, formatISO, set } = require("date-fns");
 const { createClient } = require("@supabase/supabase-js");
 const { create } = require("domain");
 
@@ -18,6 +18,10 @@ const supabase = createClient(
   NEXT_PUBLIC_SUPABASE_URL,
   SUPABASE_SERVICE_ROLE_KEY
 );
+
+function isFloatString(str) {
+  return /^-?\d+(\.\d+)?$/.test(str.trim());
+}
 
 async function searchDeals(dealName) {
   const url = new URL("https://api.hubapi.com/crm/v3/objects/deals/search");
@@ -53,7 +57,11 @@ async function searchDeals(dealName) {
 
   const json = await response.json();
 
-  return json.results.length > 0;
+  if (json.results.length === 0) {
+    return null;
+  }
+
+  return json.results[0].id;
 }
 
 function isInvalidDate(dateInput) {
@@ -61,199 +69,288 @@ function isInvalidDate(dateInput) {
   return isNaN(date.getTime()); // true = invalid, false = valid
 }
 
+async function deleteDeal(dealId) {
+  try {
+    const apiResponse = await hubspotClient.crm.deals.basicApi.archive(dealId);
+  } catch (e) {
+    e.message === "HTTP request failed"
+      ? console.error(JSON.stringify(e.response, null, 2))
+      : console.error(e);
+
+    throw e;
+  }
+}
+
+async function buildReport(report) {
+  const reportDate = parse(report.ReportDate, "dd/MM/yyyy", new Date());
+  if (isInvalidDate(reportDate)) {
+    return null;
+  }
+
+  const div43File = `output/div-43-${report.ID}.json`;
+  const div40File = `output/div-40-${report.ID}.json`;
+
+  const rawDiv43 = fs.existsSync(div43File)
+    ? JSON.parse(fs.readFileSync(div43File, "utf8"))
+    : [];
+
+  const rawDiv40 = fs.existsSync(div40File)
+    ? JSON.parse(fs.readFileSync(div40File, "utf8"))
+    : [];
+
+  const division_43 = rawDiv43.map((a) => {
+    const installation_date_formula =
+      a.InstallationDateFormula === "cc"
+        ? `=${a.InstallationDateFormula}`
+        : parseAssetDate(a.InstallationDateFormula);
+
+    const rate_formula = !isFloatString(a.RateFormula)
+      ? `=${a.RateFormula}`
+      : a.RateFormula;
+
+    const quantity_formula = !isFloatString(a.QuantityFormula)
+      ? `=${a.QuantityFormula}`
+      : a.QuantityFormula;
+
+    return {
+      name: a.Name,
+      area: a.Area === 0 ? "Unit Specific" : "Common Property",
+      installation_date_formula,
+      rate_formula,
+      quantity_formula,
+      scrapped_date: a.ScrappedDateFormula,
+      is_given_cost: a.IsGivenCost === "0" ? false : true,
+    };
+  });
+
+  const division_40 = rawDiv40.map((a) => {
+    const installation_date_formula =
+      a.InstallationDateFormula === "cc"
+        ? `=${a.InstallationDateFormula}`
+        : parseAssetDate(a.InstallationDateFormula);
+
+    const rate_formula = !isFloatString(a.RateFormula)
+      ? `=${a.RateFormula}`
+      : a.RateFormula;
+
+    const quantity_formula = !isFloatString(a.QuantityFormula)
+      ? `=${a.QuantityFormula}`
+      : a.QuantityFormula;
+
+    return {
+      name: a.Name,
+      area: a.Area === 0 ? "Unit Specific" : "Common Property",
+      installation_date_formula,
+      rate_formula,
+      quantity_formula,
+      scrapped_date: a.ScrappedDateFormula,
+      is_given_cost: a.IsGivenCost === "0" ? false : true,
+      exclude_from_division_43: a.IsDivision43Deduction === "1" ? false : true,
+      hundred_percent_option:
+        a.HundredPercentOption === 0
+          ? "Yes"
+          : a.HundredPercentOption === 1
+          ? "No"
+          : "Force",
+      number_of_items: a.NumberOfItems,
+      exclude_fees: a.ExcludeFees === "1" ? true : false,
+      exclude_expenditure: a.ExcludeExpenditure === "1" ? true : false,
+    };
+  });
+
+  const result = {
+    report_date: formatISO(reportDate, { representation: "date" }),
+    years_in_schedule: report.YearsInSchedule,
+    pre_notional_write_down_years: report.PreNotionalWriteDownYears,
+    notional_write_down_rate: report.NotionalWriteDownRate,
+    notional_write_down_governor: report.NotionalWriteDownGovernor,
+    preliminary_fees: report.PreliminaryFees,
+    reference: report.Reference,
+    expenditure_governor: report.ExpenditureGovernor,
+    consultancy_fees: report.ConsultancyFees,
+    back_claim_years: report.NumberOfYearsToBackClaim,
+    division_43,
+    division_40,
+  };
+
+  return result;
+}
+
+const parseDate = (dateString) => {
+  const [day, month, year] = dateString.split("/");
+
+  const actualDay = day.length === 1 ? `0${day}` : day;
+  const actualMonth = month.length === 1 ? `0${month}` : month;
+  let actualYear = "";
+
+  if (year.length === 4) {
+    actualYear = year;
+  } else {
+    const yearNumber = parseInt(year, 10);
+    actualYear = yearNumber < 50 ? `20${year}` : `19${year}`;
+  }
+
+  const formattedDateString = `${actualDay}/${actualMonth}/${actualYear}`;
+
+  const date = parse(formattedDateString, "dd/MM/yyyy", new Date());
+  return isInvalidDate(date)
+    ? null
+    : formatISO(date, { representation: "date" });
+};
+
+const parseAssetDate = (dateString) => {
+  const [day, month, year] = dateString.split("/");
+
+  const actualDay = day.length === 1 ? `0${day}` : day;
+  const actualMonth = month.length === 1 ? `0${month}` : month;
+  let actualYear = "";
+
+  if (year.length === 4) {
+    actualYear = year;
+  } else {
+    const yearNumber = parseInt(year, 10);
+    actualYear = yearNumber < 50 ? `20${year}` : `19${year}`;
+  }
+
+  const formattedDateString = `${actualDay}/${actualMonth}/${actualYear}`;
+  return formattedDateString;
+};
+
+async function buildDeal(report) {
+  const ccd = parseDate(report.ConstructionCompletion);
+  const settlement_date = parseDate(report.Settlement);
+  const common_entitlement = parseFloat(report.CommonEntitlementFormula);
+
+  const first_use_date =
+    report.AvailableFirstUseFormula === "settlement"
+      ? settlement_date
+      : parseDate(report.AvailableFirstUseFormula);
+
+  if (
+    !ccd ||
+    !settlement_date ||
+    !first_use_date ||
+    isNaN(common_entitlement)
+  ) {
+    return null;
+  }
+
+  const deal = {
+    settlement_date,
+    ccd,
+    first_use_date,
+    dealname: report.Reference,
+    purchase_price: report.PurchasePrice,
+    property_type: "Unit",
+    common_entitlement,
+    land_value: report.LandValue,
+    number_of_levels: report.NumberOfLevels,
+    number_of_units: report.NumberOfUnits,
+    strata_provider: report.StrataPlanProvider,
+    verbal_info: report.VerbalInformationProvidedBy,
+    written_info: report.WrittenInformationProvidedBy,
+    council: report.CouncilName,
+    // is_brand_new:
+  };
+
+  return deal;
+}
+
+async function createDeal(deal) {
+  const config = {
+    properties: deal,
+  };
+
+  try {
+    const apiResponse = await hubspotClient.crm.deals.basicApi.create(config);
+
+    return apiResponse.id;
+  } catch (e) {
+    e.message === "HTTP request failed"
+      ? console.error(JSON.stringify(e.response, null, 2))
+      : console.error(e);
+
+    console.error(deal);
+    throw e;
+
+    return null;
+  }
+}
+
+async function createReport({ division_40, division_43, ...report }) {
+  const reportExists = await supabase
+    .from("reports")
+    .select("id")
+    .eq("reference", report.reference)
+    .maybeSingle();
+
+  if (reportExists.data) {
+    await supabase.from("reports").delete().eq("id", reportExists.data.id);
+  }
+
+  const { data, error } = await supabase
+    .from("reports")
+    .insert(report)
+    .select("id")
+    .single()
+    .throwOnError();
+
+  await supabase
+    .from("division_40_assets")
+    .insert(
+      division_40.map((item) => ({
+        ...item,
+        report_id: data.id,
+      }))
+    )
+    .throwOnError();
+
+  await supabase
+    .from("division_43_assets")
+    .insert(
+      division_43.map((item) => ({
+        ...item,
+        report_id: data.id,
+      }))
+    )
+    .throwOnError();
+}
+
 async function run() {
   const reports = JSON.parse(fs.readFileSync("reports.json", "utf8"));
 
-  for (const r of reports) {
-    const exists = await searchDeals(r.Reference);
+  for (let i = 0; i < 20; i++) {
+    //   for (let i = 0; i < reports.length; i++) {
+    const r = reports[i];
+    console.log(`Processing report: ${r.Reference}`);
+    await new Promise((resolve) => setTimeout(resolve, 300));
+    const existingId = await searchDeals(r.Reference);
 
-    if (exists) {
-      continue;
+    if (existingId) {
+      await deleteDeal(existingId);
     }
 
     const deal = await buildDeal(r);
     const report = await buildReport(r);
 
-    console.log(deal, report);
-
     if (!deal || !report) {
-      console.error(
+      console.log(
         `Skipping report ${report.Reference} due to invalid dates or missing data`
       );
       continue;
     }
 
     const dealId = await createDeal(deal);
-    console.log(dealId);
-    createReport({ ...report, hubspot_deal_id: dealId });
+    await createReport({ ...report, hubspot_deal_id: dealId });
 
     console.log(`Created deal for report ${r.Reference} with ID ${dealId}`);
-
-    return;
-  }
-
-  async function buildReport(report) {
-    const reportDate = parse(report.ReportDate, "dd/MM/yyyy", new Date());
-    if (isInvalidDate(reportDate)) {
-      return null;
-    }
-
-    const result = {
-      report_date: formatISO(reportDate, { representation: "date" }),
-      years_in_schedule: report.YearsInSchedule,
-      pre_notional_write_down_years: report.PreNotionalWriteDownYears,
-      notional_write_down_rate: report.NotionalWriteDownRate,
-      notional_write_down_governor: report.NotionalWriteDownGovernor,
-      preliminary_fees: report.PreliminaryFees,
-      reference: report.Reference,
-      expenditure_governor: report.ExpenditureGovernor,
-      consultancy_fees: report.ConsultancyFees,
-      back_claim_years: report.NumberOfYearsToBackClaim,
-    };
-
-    return result;
-  }
-
-  async function buildDeal(report) {
-    const ccd = parse(report.ConstructionCompletion, "dd/MM/yyyy", new Date());
-    const settlement = parse(report.Settlement, "dd/MM/yyyy", new Date());
-    const common_entitlement = parseFloat(report.CommonEntitlementFormula);
-
-    const firstUse =
-      report.AvailableFirstUseFormula === "settlement"
-        ? settlement
-        : parse(report.AvailableFirstUseFormula, "dd/MM/yyyy", new Date());
-
-    if (
-      isInvalidDate(ccd) ||
-      isInvalidDate(settlement) ||
-      isInvalidDate(firstUse) ||
-      isNaN(common_entitlement)
-    ) {
-      return null;
-    }
-
-    const deal = {
-      settlement_date: formatISO(settlement, { representation: "date" }),
-      ccd: formatISO(ccd, { representation: "date" }),
-      first_use_date: formatISO(firstUse, { representation: "date" }),
-      dealname: report.Reference,
-      purchase_price: report.PurchasePrice,
-      property_type: "Unit",
-      common_entitlement,
-      land_value: report.LandValue,
-      number_of_levels: report.NumberOfLevels,
-      number_of_units: report.NumberOfUnits,
-      strata_provider: report.StrataPlanProvider,
-      verbal_info: report.VerbalInformationProvidedBy,
-      written_info: report.WrittenInformationProvidedBy,
-      council: report.CouncilName,
-      // is_brand_new:
-    };
-
-    return deal;
-  }
-
-  async function createDeal(deal) {
-    const config = {
-      properties: deal,
-    };
-
-    try {
-      const apiResponse = await hubspotClient.crm.deals.basicApi.create(config);
-
-      return apiResponse.id;
-    } catch (e) {
-      e.message === "HTTP request failed"
-        ? console.error(JSON.stringify(e.response, null, 2))
-        : console.error(e);
-
-      return null;
-    }
-  }
-}
-
-async function createReport(report) {
-  const { data, error } = await supabase
-    .from("reports")
-    .upsert(report, { onConflict: ["reference"] });
-
-  if (error) {
-    console.error("Error creating report:", error);
-    return null;
   }
 }
 
 run();
 
+// console.log(parseFloat('44006.77+4000.62'));
+
 /*
 
-  id: z.string(),
-  dealname: z.string(),
-  council: z.string().nullish(),
-  settlement_date: z.coerce.date().nullish(),
-  site_inspection_date: z.coerce.date().nullish(),
-  purchase_price: z.coerce.number().nullish(),
-  property_type: z.string({ description: 'Property Type', message: 'Property type is required.' }).min(1, { message: 'Property type is required' }),
-  property_street_address: z.string().nullish(),
-  property_suburb: z.string().nullish(),
-  property_state: z.string().nullish(),
-  property_postcode: z.string().nullish(),
-  ccd: z.coerce.date().nullish(),
-  common_entitlement: z.coerce.number().nullish(),
-  first_use_date: z.coerce.date().nullish(),
-  land_value: z.coerce.number().nullish(),
-  number_of_levels: z.coerce.number().nullish(),
-  number_of_units: z.coerce.number().nullish(),
-  property_owner_1_entity: z.string().nullish(),
-  property_owner_1_first_name: z.string().nullish(),
-  property_owner_1_last_name: z.string().nullish(),
-  property_owner_1_ownership: z.coerce.number().nullish(),
-  property_owner_2_entity: z.string().nullish(),
-  property_owner_2_first_name: z.string().nullish(),
-  property_owner_2_last_name: z.string().nullish(),
-  property_owner_2_ownership: z.coerce.number().nullish(),
-  property_owner_3_entity: z.string().nullish(),
-  property_owner_3_first_name: z.string().nullish(),
-  property_owner_3_last_name: z.string().nullish(),
-  property_owner_3_ownership: z.coerce.number().nullish(),
-  property_owner_4_entity: z.string().nullish(),
-  property_owner_4_first_name: z.string().nullish(),
-  property_owner_4_last_name: z.string().nullish(),
-  property_owner_4_ownership: z.coerce.number().nullish(),
-  strata_provider: z.string().nullish(),
-  verbal_info: z.string().nullish(),
-  written_info: z.string().nullish(),
-  is_brand_new: z.boolean().nullish(),
- 
-
-  if (!HUBSPOT_TOKEN) {
-    throw new Error('HubSpot token not set in environment variables');
-  }
-
-  const url = new URL(`https://api.hubapi.com/crm/v3/objects/deals/${dealId}`);
-  url.searchParams.set('properties', propertyNames.join(','));
-
-  const response = await fetch(url.toString(), {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${HUBSPOT_TOKEN}`,
-      'Content-Type': 'application/json',
-    },
-    cache: 'no-store',
-  });
-
-  if (!response.ok) {
-    if (response.status === 404) {
-      return notFound()
-    }
-
-    const errorBody = await response.text();
-    throw new Error(`Failed to fetch deal: ${response.status} ${errorBody}`);
-  }
-
-  const data = await response.json();
-  if (!data || !data.properties) {
-    throw new Error('Deal data is missing properties');
-  }
 */
